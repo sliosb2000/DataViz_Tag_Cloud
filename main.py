ce253009aa244cb37479667ee83c73bcb96be801
anarchopossum <jasminesj@proton.me>
2022-09-18 21:56:56 -0700

    WIP: Making the project count all the words in the document
    I am receiving an error:
    ```
    line 15, in <module>
        dic[word] = 1
    TypeError: unhashable type: 'list'
    ```
    I hope to get this running soon but after this i will need to distinguish from punctuation.

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import csv
+from distutils.command.install import key
 
-with open('input.csv') as info_file:
-    pass
+with open('input.txt', 'r') as info_file:
+    dic = dict()
+
+    for line in info_file:
+        line = line.strip()
+        line = line.upper()
+        word = line.split()
+
+        for w in dic:
+            dic[w] = dic[w] + 1
+        else:
+            dic[word] = 1
+
+    for word in list(dic.keys()):
+        print(key, " ", dic[key])
