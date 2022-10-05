e133c9c70a8de664505251aa0cee1eb3d308e87a
anarchopossum <jasmine.sanjuan01@student.csulb.edu>
2022-10-05 15:11:06 -0700

    init
    Basically modified the HTML generator to allow for several charts and also am currently working on getting those SVGs formatted properly

--- genCloud.py
+++ genCloud.py
@@ -16,9 +16,9 @@ def wordcloud_gen(folder):  # This is the Head function of the whole project
     filteredWords = get_words_and_frequencies_dictionary_above_weight(counts, 1.0)  # finds freq of said words
     info = wordcloud_svg_gen(filteredWords)  # creates the SVG of the word cloud
     #info = svg_tooltip_gen(info, filteredWords)  # modifies the SVG info to add tooltip data # Obscolete, the svg is build in html_builder()
-    html_name = folder + ".html"  # gives html file output name
+    html_name = "index" + ".html"  # gives html file output name
     f = open(html_name, 'w')  # opens a new/exiting html doc and allows writing
-    html_builder(f, info)  # enters html info into opened file
+    # html_builder(f, info)  # enters html info into opened file
     f.close()
 
 
@@ -91,7 +91,7 @@ def svg_tooltip_gen(data, dictionary):
     return data
 
 
-def html_builder(file, svg_file):
+def html_builder(file, svg_file, svg_file1, svg_file2):
     Top = """
     <html>
         <head>
@@ -103,26 +103,63 @@ def html_builder(file, svg_file):
         </style>
         </head>
         <body>
+            <select id="choose" onchange="myFunction(this.options[this.selectedIndex].value);"> 
+        <option value="triangle">Kennedy</option>
+        <option value="square">Eisenhower</option>
+        <option value="circle">Nixon</option>
+      </select>
+    <div class="triangle" id="triangle">
     """
+    div_1 = """
+    </div>
+
+
+    <div class="square" id="square">"""
+
+    div_2 = """
+    </div>
+
+    <div class="circle" id="circle">"""
+
+
+
     Bottom = """
-        </body>
-    </html>
+</div>
+
+<script>
+    document.getElementById("triangle").style.display = "block";
+    document.getElementById("circle").style.display = "none";
+    document.getElementById("square").style.display = "none";
+    //document.getElementById("square").style.display = "none";
+  
+    function myFunction(c) 
+    {
+    document.getElementById("triangle").style.display = "none";
+    document.getElementById("circle").style.display = "none";
+    document.getElementById("square").style.display = "none";
+    document.getElementById(c).style.display = "block";
+    }
+</script>
+</body>
+</html>
     """
+    svg_list = [svg_file,svg_file1,svg_file2]
+    for x in svg_list:
+        open("temp_svg.svg","w").write(x) #there probably is a be a better idea, smoother
+
+        read = open("temp_svg.svg","r")
+        write_svg = ""
+        for line in read:
+            word =line.split(">")[1].split("<")[0]
+            if len(word)>1 and len(word)<72:      #to ignore the first three lines and empty ones
+                s = word+"<title>"+word+"</title>"
+                li = line.replace(word,s)
+                write_svg+=li
+            else:
+                write_svg += line
+        x == write_svg
     
-    open("temp_svg.svg","w").write(svg_file) #there probably is a be a better idea, smoother
-    
-    read = open("temp_svg.svg","r")
-    write_svg = ""
-    for line in read:
-        word =line.split(">")[1].split("<")[0] 
-        if len(word)>1 and len(word)<72:      #to ignore the first three lines and empty ones
-            s = word+"<title>"+word+"</title>"
-            li = line.replace(word,s)
-            write_svg+=li
-        else:
-            write_svg += line
-    
-    file.write(Top + write_svg + Bottom)
+    file.write(Top + svg_file + div_1 + svg_file1 + div_2 + svg_file2 + Bottom)
 
 
 processor = BasicWordProcessor()
@@ -142,4 +179,7 @@ processor = BasicWordProcessor()
 # # the file to our liking.
 # f = open('index.html', 'w')
 # html_builder(f,info)
-wordcloud_gen('nixon')
+Kennedy = wordcloud_gen('Kennedy')
+Nixon = wordcloud_gen('nixon')
+Eisenhower = wordcloud_gen('Eisenhower')
+html_builder("index.html", Kennedy, Nixon, Eisenhower)  # enters html info into opened file
